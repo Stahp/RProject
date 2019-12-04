508f3b0bc65a5e667e1609df09e381bd5f439063
Moussaid Yassine <moussaidine@gmail.com>
2019-12-04 16:47:01 +0100

    Add files via upload

--- ReadMe.txt
+++ ReadMe.txt
@@ -13,4 +13,13 @@ Q2:
 "Fare": Quantitative
 "Cabin": Qualitative
 "Embarked": Qualitative
--
+- is.na returns either true or false whether the value is missing or not.
+using it on the each of the colums in train and summing the result array, we will have the number of missing values.
+
+for (c in names(train))
+  print(sum(is.na(train[c])))
+
+and so, we can see that there are:
+	-463 missing values in Cabin
+	-121 missing values in Age
+	-001 missing values in Embarked
--- Solution.R
+++ Solution.R
@@ -1,4 +1,21 @@
 load('D:/My Personal Documents/Project/titanic_train.Rdata')
 
 for (c in names(train))
-  print(nrow(train[c]))
+  print(sum(is.na(train[c])))
+
+S= train["Survived"][,1]
+Sx= train["Sex"][,1]
+P= train["Pclass"][,1]
+A= train["Age"][,1]
+
+summary(S)
+summary(Sx)
+summary(P)
+summary(A)
+
+cAge= cut(A, breaks= c(0, 20, 40, 60, 80))
+summary(cAge)
+
+counts= table(S, Sx)
+barplot(counts, col=c("blue","red"), xlab="Sex", legend = rownames(counts), beside= TRUE)
+barplot(counts, col=c("blue","red"), xlab="Sex", legend = c("Didn't Survive", "Survived"), beside= TRUE)
